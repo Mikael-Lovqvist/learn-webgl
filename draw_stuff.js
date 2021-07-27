097d5267a96e69c7f9509d9a8b8b31b89117ad14
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2021-07-27 06:10:33 +0200

    Now uses vertex indicies for drawing the mesh

--- draw_stuff.js
+++ draw_stuff.js
@@ -22,17 +22,20 @@ function draw_scene() {
 
 	gl.enable(gl.DEPTH_TEST);
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, application_state.vertex_buffer);
 	//void gl.vertexAttribPointer(index, size, type, normalized, stride, offset);
 
 	//One vertex is 12 bytes so we need a stride of 24 for the normals	
-	gl.vertexAttribPointer(vert_attr.position_happy, application_state.vertex_buffer.itemSize, gl.FLOAT, false, 48, 0);
-	gl.vertexAttribPointer(vert_attr.position_sad, application_state.vertex_buffer.itemSize, gl.FLOAT, false, 48, 24);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, application_state.buffers.vertices);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, application_state.buffers.index_buffer);
+
+	gl.vertexAttribPointer(vert_attr.position_happy, application_state.buffers.vertices.itemSize, gl.FLOAT, false, 24, 0);
+	gl.vertexAttribPointer(vert_attr.position_sad, application_state.buffers.vertices.itemSize, gl.FLOAT, false, 24, application_state.buffers.happy.length*4);
 
 	//Bind normals
-	gl.vertexAttribPointer(vert_attr.normal_happy, application_state.vertex_buffer.itemSize, gl.FLOAT, false, 48, 12);
-	gl.vertexAttribPointer(vert_attr.normal_sad, application_state.vertex_buffer.itemSize, gl.FLOAT, false, 48, 36);
-	
+	gl.vertexAttribPointer(vert_attr.normal_happy, application_state.buffers.vertices.itemSize, gl.FLOAT, false, 24, 12);
+	gl.vertexAttribPointer(vert_attr.normal_sad, application_state.buffers.vertices.itemSize, gl.FLOAT, false, 24, 12+application_state.buffers.happy.length*4);
+
 	//Set matrices
 	gl.uniformMatrix4fv(vert_attr.projection_matrix, false, application_state.projection_matrix);
 	gl.uniformMatrix4fv(vert_attr.camera_matrix, false, application_state.camera_matrix);
@@ -41,11 +44,12 @@ function draw_scene() {
 	gl.uniform1f(vert_attr.interpolation_value, Math.sin(window.performance.now()*1e-3)*0.7+0.5);	//We go a bit out of bounds for the fun of it
 
 	//Set light direction	
-	gl.uniform3f(vert_attr.light_direction, -0.6, 0.3, -0.4);
+	gl.uniform3f(vert_attr.light_direction, -0.4, 0.4, -0.8);
 	
 
 	//Drawcall	
-	gl.drawArrays(gl.TRIANGLES, 0, application_state.triangle_count*3);
+	gl.drawElements(gl.TRIANGLES, application_state.buffers.face_polygons.length, gl.UNSIGNED_SHORT, 0);
+
 
 	window.requestAnimationFrame(draw_scene);
 
