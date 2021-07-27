8d7bbb25b7eebc664c4b5cb507d66a1d54d26ec5
Mikael Lövqvist <mikael.lovqvist.mail@gmail.com>
2021-07-27 06:09:28 +0200

    Updated experiment to utilize the new methods of mesh loading

--- main_experiment.js
+++ main_experiment.js
@@ -12,6 +12,43 @@ function start_experiment() {
 
 
 	function check_if_everything_is_ready() {
+
+
+		if (application_state.happy_vertices_ready && application_state.sad_vertices_ready && application_state.face_polygons_ready && !application_state.meshes_ready) {
+			
+			if (application_state.buffers.happy.length != application_state.buffers.sad.length) {
+				throw new Error("Vertex count mismatch between sad and happy face!");
+			}
+			
+			const vbuf = gl.createBuffer();
+			const fa = new Float32Array(application_state.buffers.happy.length + application_state.buffers.sad.length);
+			
+			fa.set(application_state.buffers.happy, 0);
+			fa.set(application_state.buffers.sad, application_state.buffers.happy.length);
+
+
+			gl.bindBuffer(gl.ARRAY_BUFFER, vbuf);
+			vbuf.itemSize = 3
+			vbuf.numItems = fa.length;
+			gl.bufferData(gl.ARRAY_BUFFER, fa, gl.STATIC_DRAW);
+			application_state.buffers.vertices = vbuf;
+			
+
+
+			const ibuf = gl.createBuffer();
+			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibuf);
+			gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, application_state.buffers.face_polygons, gl.STATIC_DRAW);
+
+			application_state.buffers.index_buffer = ibuf;
+
+
+
+
+			application_state.meshes_ready = true;
+		}
+
+
+
 		if (application_state.meshes_ready && application_state.shaders_ready) {
 			status.innerText += "Let's rock!\n";
 
@@ -19,7 +56,7 @@ function start_experiment() {
 
 		}
 	}
-
+/*
 	function check_if_meshes_are_loaded() {
 		if (happy_mesh && sad_mesh && application_state.gl) {
 			status.innerText += 'Mesh load complete\nCreating buffers\n';
@@ -28,7 +65,7 @@ function start_experiment() {
 			application_state.meshes_ready = true;
 			check_if_everything_is_ready();
 		}
-	}
+	}*/
 
 	function check_if_shaders_are_loaded() {
 		if (application_state.vertex_shader && application_state.fragment_shader) {
@@ -43,7 +80,7 @@ function start_experiment() {
 		}
 	}
 
-
+/*
 	get_stl('happy.stl').then(mesh => {
 		happy_mesh = mesh;
 		status.innerText += 'Happy mesh loaded\n';
@@ -54,7 +91,26 @@ function start_experiment() {
 		sad_mesh = mesh;
 		status.innerText += 'Sad mesh loaded\n';
 		check_if_meshes_are_loaded();
-	});	
+	});	*/
+
+
+	load_vertex_data('happy.vertices', 'happy').then(() => {
+		status.innerText += "Loaded happy vertices\n";
+		application_state.happy_vertices_ready = true;
+		check_if_everything_is_ready();
+	})
+
+	load_vertex_data('sad.vertices', 'sad').then(() => {
+		status.innerText += "Loaded sad vertices\n";
+		application_state.sad_vertices_ready = true;
+		check_if_everything_is_ready();
+	})
+
+	load_polygon_data('face.polygons', 'face_polygons').then(polygons => {
+		status.innerText += "Loaded face polygons\n";
+		application_state.face_polygons_ready = true;
+		check_if_everything_is_ready();
+	})
 
 	get_shader('t1.frag', application_state.gl.FRAGMENT_SHADER).then(shader => {
 		status.innerText += 'Fragment shader loaded\n';
